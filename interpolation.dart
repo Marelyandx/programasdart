f26c6e740df23902573f9799f00a61ab973553fb
Marelyandx <marelya.gomez@cbtis265.edu.mx>
2026-02-26 14:56:10 -0600

    ejercicios1
    jueves 25 de febrero

---
+++ interpolation.dart
@@ -0,0 +1,5 @@
+void main(){
+
+var firstName="Jhon";
+var lastName="Doe";
+print("Full name is $firstName $lastName");}
---
+++ tiposdedatos.dart
@@ -0,0 +1,9 @@
+void main(){
+
+var firstName="Marely Andrea";
+var lastName="Gómez Martínez ";
+var direccion="Calle Insurgentes #145 Colonoia Adolfo Lpez Mateos";
+var edad="18";
+var altu="1.55cm";
+var civ="soltero";
+print("Mi nombre completo es: $firstName $lastName, mi dirección es:$direccion, mi edad es: $edad,mi altura es: $altu, y mi estado civil es: $civ");}
